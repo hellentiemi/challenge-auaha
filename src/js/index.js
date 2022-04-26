81a520a66488b36c0e20e6f0e5fa35419874c69a
Hellen <hellen.tiemi.chiba@gmail.com>
2022-04-26 10:30:17 -0300

    commit projeto

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.banner-carrosel').slick({
+        dots: true
+    });
+});
+
+$(function() {
+    $('.slider-produtos').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+});
